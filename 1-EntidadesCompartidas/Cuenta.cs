16d9389c6a6d363c3f8713cc736ff03a956d894c
German <German@DESKTOP-OVQNAPT>
2018-11-02 21:55:25 -0300

    Add project files.

---
+++ 1-EntidadesCompartidas/Cuenta.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntidadesCompartidas
+{
+   public class Cuenta
+    {
+
+       // atributos
+       private int _NumCta;
+       private Cliente _unCliente;
+       private double _SaldoCta;
+
+
+       //propiedades
+       public int NumCta
+       {
+           get { return _NumCta; }
+           set { _NumCta = value; }
+       }
+
+       public Cliente UnCliente
+       {
+           get { return _unCliente; }
+           set { _unCliente = value; }
+       }
+
+       public double SaldoCuenta
+       {
+           get { return _SaldoCta; }
+       }
+
+       public virtual string TipoCuenta
+       {
+           get { return "No se";}
+       }
+
+
+       //constructores
+       public Cuenta(int pNumCta, Cliente pUnCliente, double pSaldoCuenta)
+       {
+           NumCta = pNumCta;
+           UnCliente = pUnCliente;
+           _SaldoCta = pSaldoCuenta;
+       }
+
+       public override string ToString()
+       {
+           return (this.NumCta.ToString().Trim() + " - " + this.UnCliente.NomCli + " - " + this.TipoCuenta);
+       }
+ 
+    }
+}
---
+++ 2-Logica/FabricaLogica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logica
+{
+   public class FabricaLogica
+    {
+       public static ILogicaCliente GetLogicaCliente()
+       {
+           return (LogicaCliente.GetInstancia());
+       }
+
+       public static ILogicaCuenta GetLogicaCuenta()
+       {
+           return (LogicaCuenta.GetInstancia());
+       }
+
+       public static ILogicaMovimiento GetLogicaMovimiento()
+       {
+           return (LogicaMovimiento.GetInstancia());
+       }
+
+       public static ILogicaEmpleado GetLogicaEmpleado()
+       {
+           return (LogicaEmpleado.GetInstancia());
+       }
+
+    }
+}
